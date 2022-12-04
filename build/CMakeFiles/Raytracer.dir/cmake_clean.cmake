aa7d975af23e4dab676802735dff74b712d5cfce
Daniel <danielweissenborn@outlook.de>
2022-12-04 19:21:19 +0100

    1st commit exercise 5

---
+++ build/CMakeFiles/Raytracer.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Raytracer.dir/main.cpp.obj"
+  "CMakeFiles/Raytracer.dir/main.cpp.obj.d"
+  "CMakeFiles/Raytracer.dir/sceneobject.cpp.obj"
+  "CMakeFiles/Raytracer.dir/sceneobject.cpp.obj.d"
+  "Raytracer.exe"
+  "Raytracer.exe.manifest"
+  "Raytracer.pdb"
+  "libRaytracer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Raytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
